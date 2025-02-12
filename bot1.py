05bfc4423f7430a782511e4d1d8e17a4bebcc564
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2025-02-12 17:03:54 +0100

    test

--- bot1.py
+++ bot1.py
@@ -37,17 +37,18 @@ class sma_daytrader_bot():
         if cp == sma:
             return "hold",stre
 
-bot = sma_daytrader_bot(["AAPL"])
+symbol = input("what do you want to trade?")
+bot = sma_daytrader_bot([symbol])
 simulator = sim(10)
 counter = 0
 while True:
     if counter == 120:
-        sig = bot.signal("AAPL")
+        sig = bot.signal(symbol)
         print(sig)
         counter = 0
         x = input("DO IT(Y("")/N) or c to stop simulation")
         if x == "" or x.capitalize() == "Y":
-            simulator.execute_bot(round(sig[1]),"AAPL",sig[0])
+            simulator.execute_bot(round(sig[1]),symbol,sig[0])
         if x.lower() == "c":
             break
     counter += 1
