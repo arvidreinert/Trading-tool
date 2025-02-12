7b1fd7108e89459a84d58d5a023daec37753b2dc
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2025-02-12 15:57:01 +0100

    WIP

--- bot1.py
+++ bot1.py
@@ -3,15 +3,22 @@ import finance_data as fd
 from fisims import sim
 
 class sma_bot():
-    def __init__(self,stocks):
+    def __init__(self,stocks_allowed):
         self.chunk = rd.data_chunk()
         self.boughts = {}
-        for stock in stocks:
+        for stock in stocks_allowed:
             self.chunk.add_symbol(stock)
             self.boughts[stock] = 0
 
-    def signal(bought=False):
-        pass
+    def is_bought(self,stock):
+        cond = False
+        if stock in self.boughts:
+            if self.boughts[stock] > 0:
+                cond = True
+        return cond
+
+    def signal(self,stock,bought=False):
+        sma = self.chunk
 
 bot = sma_bot(["AAPL"])
 simulator = sim(10)
