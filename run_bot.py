c1560474cec91a85e19a03d8bda4435641c5dca0
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2025-02-13 17:17:46 +0100

    adding features

--- run_bot.py
+++ run_bot.py
@@ -1,7 +1,7 @@
 from bot1 import *
 import time
 
-symbols = ["GOOG"]
+symbols = ["GOOG","AAPL","UBER","GAN"]
 bot = sma_daytrader_bot(symbols)
 simulator = sim(10)
 counter = 0
@@ -13,13 +13,17 @@ try:
             time.sleep(1)
             sig = bot.signal(symboll)
             if len(sig) >= 5:
-                print(f"{symboll}:{sig[0]},price:{sig[1]},gain/loss:{sig[3]} or {round(float(sig[3])/float(sig[1]),4)}%")
+                print(f"{symboll}:{sig[0]},price:{sig[1]},gain/loss:{sig[3]} or {round(float(sig[3])/float(sig[1])*100,4)}%")
             if sig[0] == "buy":
                 print(simulator.execute_bot(1,symboll,sig[0]))
             if sig[0] == "sell":
                 print(simulator.execute_bot(simulator.stocks[symboll]["longs"],symboll,"sell"))
-                break
 except:
     end_time = time.perf_counter()
+    for symboll in symbols:
+        try:
+            print(simulator.execute_bot(simulator.stocks[symboll]["longs"],symboll,"sell"))
+        except:
+            pass
     print(f"stopped after {end_time - start_time} seconds. Thats {(end_time - start_time)/60} minutes")
     simulator.portfolio()
