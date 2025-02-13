06583a63657954b24cb8fc57359c00a85da19afe
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2025-02-13 11:40:24 +0100

    WIP

--- bot1.py
+++ bot1.py
@@ -35,19 +35,3 @@ class sma_daytrader_bot():
                 return "sell",stre
         if cp == sma:
             return "hold",stre
-
-symbol = input("what do you want to trade?")
-bot = sma_daytrader_bot([symbol])
-simulator = sim(10)
-counter = 0
-while True:
-    if counter == 120:
-        sig = bot.signal(symbol)
-        print(sig)
-        counter = 0
-        x = input("DO IT(Y("")/N) or c to stop simulation")
-        if x == "" or x.capitalize() == "Y":
-            simulator.execute_bot(round(sig[1]),symbol,sig[0])
-        if x.lower() == "c":
-            break
-    counter += 1
--- run_bot.py
+++ run_bot.py
@@ -1,17 +1,14 @@
 from bot1 import *
 
-symbol = input("what do you want to trade?")
-bot = sma_daytrader_bot([symbol])
+symbols = ["AAPL","GOOG","NVDA","MSFT","META"]
+bot = sma_daytrader_bot([symbols])
 simulator = sim(10)
 counter = 0
 while True:
     if counter == 120:
-        sig = bot.signal(symbol)
-        print(sig)
+        for symbol in symbols:
+            sig = bot.signal(symbol)
+            print(sig)
+            print(simulator.execute_bot(round(sig[1]),symbol,sig[0]))
         counter = 0
-        x = input("DO IT(Y("")/N) or c to stop simulation")
-        if x == "" or x.capitalize() == "Y":
-            simulator.execute_bot(round(sig[1]),symbol,sig[0])
-        if x.lower() == "c":
-            break
     counter += 1
